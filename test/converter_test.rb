148dc95678c371b5af628511c4fd6edc9237cbf2
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-05-24 01:33:48 +0300

    Tests for reversed ranges

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -14,5 +14,12 @@ describe RangeRegexp::Converter do
       ensure_correct_convertation(-2..0, /-[1-2]|0/)
       ensure_correct_convertation(-3..1, /-[1-3]|[0-1]/)
     end
+    
+    it 'works as expected for reversed ranges' do
+      ensure_correct_convertation(9..-9, /-[1-9]|\d/)
+      ensure_correct_convertation(3456..12, /1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]/)
+      ensure_correct_convertation(0..-2, /-[1-2]|0/)
+      ensure_correct_convertation(1..-3, /-[1-3]|[0-1]/)
+    end   
   end
 end

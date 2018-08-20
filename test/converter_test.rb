22c6ac74b45f41eff8ba2b21ad38d3d397e85a76
colin <H2597@token.co.jp>
2018-08-20 11:09:04 +0900

    fixes issues with anchors, and adds support for end excluded ranges

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -14,34 +14,34 @@ describe RangeRegexp::Converter do
     end
 
     it 'returns a range representation as a regexp' do
-      ensure_correct_conversion(-9..9, /-[1-9]|\d/)
-      ensure_correct_conversion(12..3456, /1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]/)
-      ensure_correct_conversion(-2..0, /-[1-2]|0/)
-      ensure_correct_conversion(-3..1, /-[1-3]|[0-1]/)
+      ensure_correct_conversion(-9..9, /(-[1-9]|\d)/)
+      ensure_correct_conversion(12..3456, /(1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6])/)
+      ensure_correct_conversion(-2..0, /(-[1-2]|0)/)
+      ensure_correct_conversion(-3..1, /(-[1-3]|[0-1])/)
     end
 
     it 'works as expected for reversed ranges' do
-      ensure_correct_conversion(9..-9, /-[1-9]|\d/)
-      ensure_correct_conversion(3456..12, /1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]/)
-      ensure_correct_conversion(0..-2, /-[1-2]|0/)
-      ensure_correct_conversion(1..-3, /-[1-3]|[0-1]/)
+      ensure_correct_conversion(9..-9, /(-[1-9]|\d)/)
+      ensure_correct_conversion(3456..12, /(1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6])/)
+      ensure_correct_conversion(0..-2, /(-[1-2]|0)/)
+      ensure_correct_conversion(1..-3, /(-[1-3]|[0-1])/)
     end
 
     it 'processes options correctly' do
-      ensure_correct_conversion(-9..9, /^-[1-9]|\d$/, anchor:[:start, :end])
-      ensure_correct_conversion(3456..12, /^1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]$/, anchor: [:start, :end])
-      ensure_correct_conversion(0..-2, /^-[1-2]|0$/, anchor: [:start, :end])
-      ensure_correct_conversion(1..-3, /^-[1-3]|[0-1]$/, anchor: [:start, :end])
+      ensure_correct_conversion(-9..9, /^(-[1-9]|\d)$/, anchor:[:start, :end])
+      ensure_correct_conversion(3456..12, /^(1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6])$/, anchor: [:start, :end])
+      ensure_correct_conversion(0..-2, /^(-[1-2]|0)$/, anchor: [:start, :end])
+      ensure_correct_conversion(1..-3, /^(-[1-3]|[0-1])$/, anchor: [:start, :end])
 
-      ensure_correct_conversion(-9..9, /^-[1-9]|\d/, anchor: :start)
-      ensure_correct_conversion(3456..12, /^1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]/, anchor: :start)
-      ensure_correct_conversion(0..-2, /^-[1-2]|0/, anchor: :start)
-      ensure_correct_conversion(1..-3, /^-[1-3]|[0-1]/, anchor: :start)
+      ensure_correct_conversion(-9..9, /^(-[1-9]|\d)/, anchor: :start)
+      ensure_correct_conversion(3456..12, /^(1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6])/, anchor: :start)
+      ensure_correct_conversion(0..-2, /^(-[1-2]|0)/, anchor: :start)
+      ensure_correct_conversion(1..-3, /^(-[1-3]|[0-1])/, anchor: :start)
 
-      ensure_correct_conversion(-9..9, /-[1-9]|\d$/, anchor: :end)
-      ensure_correct_conversion(3456..12, /1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6]$/, anchor: :end)
-      ensure_correct_conversion(0..-2, /-[1-2]|0$/, anchor: :end)
-      ensure_correct_conversion(1..-3, /-[1-3]|[0-1]$/, anchor: :end)
+      ensure_correct_conversion(-9..9, /(-[1-9]|\d)$/, anchor: :end)
+      ensure_correct_conversion(3456..12, /(1[2-9]|[2-9]\d|[1-9]\d{2}|[1-2]\d{3}|3[0-3]\d{2}|34[0-4]\d|345[0-6])$/, anchor: :end)
+      ensure_correct_conversion(0..-2, /(-[1-2]|0)$/, anchor: :end)
+      ensure_correct_conversion(1..-3, /(-[1-3]|[0-1])$/, anchor: :end)
     end
 
     it 'evaluates strings against regular expression as expected in common cases' do
